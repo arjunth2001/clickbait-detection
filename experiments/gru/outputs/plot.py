2a126a2cb3c4276eea4026d88624e4f046682cc0
arvindh75 <iiith@arvindharun.com>
2021-11-30 15:19:21 +0530

    GRU outputs corrected

--- experiments/gru/outputs/plot.py
+++ experiments/gru/outputs/plot.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 loss_train, loss_val = [], []
 EPOCHS = 0
 
-with open(f'out.txt', 'r') as f:
-    for line in f:
-        sp = line.split(' ')
-        loss_train.append(float(sp[5]))
-        loss_val.append(float(sp[9]))
-        EPOCHS += 1
+for i in range(1, 3):
+    with open(f'out{i}.txt', 'r') as f:
+        for line in f:
+            sp = line.split(' ')
+            loss_train.append(float(sp[5]))
+            loss_val.append(float(sp[9]))
+            EPOCHS += 1
 
 epochs = range(1, EPOCHS + 1)
 plt.plot(epochs, loss_train, 'g', label='Training loss')
